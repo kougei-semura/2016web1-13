f2e0bd92902191adad22837d4896d0b8ca7867c6
kougei-semura <g1527044@st.t-kougei.ac.jp>
2016-07-04 17:19:21 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -10,8 +10,11 @@ var separate_time = function(time){
 
 
 var now =new Date();
-var counter = separate_time(now);
+var target = new Date(2020,7.24,0,0,0,0);
+var diff = target.getTime() - now.getTime();
+var counter = separate_time(diff);
 document.getElementById('countdown').textContent =
+'東京オリンピックまであと ' +
 counter[5] + '年' +
 counter[4] + '月' +
 counter[3] + '日' +
