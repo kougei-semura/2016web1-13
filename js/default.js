0f23dbed34c8a8e5b917c11cb3fd6c81a1f19f72
kougei-semura <g1527044@st.t-kougei.ac.jp>
2016-07-04 17:35:27 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -20,6 +20,19 @@ counter[0] + '秒';
 refresh();
 }
 
+function getFileName(){
+  return window.location.href.split('/').pop();
+}
+
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+  opt = document.querySelector('option[value="other.html"]');
+}else{
+  opt = document.querySelector('option[vlaue="index.html"]');
+}
+opt.selected = true;
+
 document.getElementById('form').select.onchange =function(){
   location.href = document.getElementById('form').select.value;
 }
