eb1cecbba4b9963e2dca26e2594271ecaa3caa55
kougei-semura <g1527044@st.t-kougei.ac.jp>
2016-07-04 17:16:02 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -12,7 +12,7 @@ var separate_time = function(time){
 var now =new Date();
 var counter = separate_time(now);
 document.getElementById(`countdown`).textContent =
-counter[5] + '年''+
+counter[5] + '年' +
 counter[4] + '月' +
 counter[3] + '日' +
 counter[2] + '時' +
