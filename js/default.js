5315fe25b5e27b32a0942ce3f7b64fac99356811
kougei-semura <g1527044@st.t-kougei.ac.jp>
2016-07-04 17:25:38 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,8 +1,8 @@
 var separate_time = function(time){
-  var sec = time.getSeconds();
-  var min = time.getMinutes();
-  var hours = time.getHours();
-  var days = time.getDate();
+  var sec   = Math.floor((time / 1000) % 60);
+  var min   = Math.floor((time / 1000 / 60) % 60);
+  var hours = Math.floor((time / 1000 / 60 / 60) % 24);
+  var days  = Math.floor((time / 1000 / 60 / 60 / 24);
   return [sec, min, hour, days];
 }
 
