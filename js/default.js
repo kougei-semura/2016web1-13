54c70b2e8421b5747b1c0ef62ada0fd20e1bd258
kougei-semura <g1527044@st.t-kougei.ac.jp>
2016-07-04 17:58:16 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -26,7 +26,7 @@ var separate_time = function(time){
 }
 
 var update = function(){
-var now =new Date();
+var now = new Date();
 var target = new Date(2020,7.24,0,0,0,0);
 var diff = target.getTime() - now.getTime();
 var counter = separate_time(diff);
